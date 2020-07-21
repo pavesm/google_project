b844677e96a4e0fefcba737dbddae9f0018e5c42
pavesm <pm@sem-wolf.org>
2020-07-21 21:00:07 +0300

    return the five common sentences

---
+++ auto_complition.py
@@ -0,0 +1,30 @@
+from collections import defaultdict
+
+subs = defaultdict(set)
+allSentences = ["hello big and wonderful world", "hello world"]
+
+
+def insertToDict(sentence):
+    for i in range(len(sentence)):
+        subs[sentence[:i + 1]].add(allSentences.index(sentence))
+        subs[sentence[i:]].add(allSentences.index(sentence))
+
+
+def fiveAutoComplition():
+    string = input("The system is ready. Enter your text:")
+    indexes = getCommonSentences(subs[string])
+    for index in indexes:
+        print(allSentences[index])
+
+
+def getCommonSentences(sentencesIndexes):
+    if len(sentencesIndexes) <= 5:
+        return list(sentencesIndexes)
+
+    return list(sentencesIndexes)[:5]
+
+
+for sentence in allSentences:
+    insertToDict(sentence)
+
+fiveAutoComplition()
