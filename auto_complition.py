f8c6f9c0345f74771ce3431dadc0f089018ab983
pavesm <pm@sem-wolf.org>
2020-07-22 00:07:42 +0300

    auto_complete_data class

--- auto_complition.py
+++ auto_complition.py
@@ -1,13 +1,15 @@
 from collections import defaultdict
+from auto_complete_data import AutoCompleteData
+
 
 subs = defaultdict(set)
-allSentences = ["hello big and wonderful world", "hello world"]
+all_sentences = [("hello big and wonderful world", "p.css"), ("hello world", "hello.html")]
 
 
-def insertToDict(sentence):
+def insert_to_dict(sentence):
     for i in range(len(sentence)):
-        subs[sentence[:i + 1]].add(allSentences.index(sentence))
-        subs[sentence[i:]].add(allSentences.index(sentence))
+        subs[sentence[:i + 1]].add(all_sentences.index(sentence))
+        subs[sentence[i:]].add(all_sentences.index(sentence))
 
     length = len(sentence)
 
@@ -16,24 +18,35 @@ def insertToDict(sentence):
             if j > length - i:
                break
 
-            subs[sentence[j:length - i]].add(allSentences.index(sentence))
+            subs[sentence[j:length - i]].add(all_sentences.index(sentence))
 
 
-def fiveAutoComplition():
-    string = input("The system is ready. Enter your text:")
-    indexes = getCommonSentences(subs[string])
+def getScore(sub_string, completed_sentence):
+    if sub_string in completed_sentence:
+        return len(sub_string) * 2
+
+
+def get_best_k_completions(sub_string):
+    indexes = get_common_sentences(subs[sub_string])
+    AutoCompleteDataList = []
     for index in indexes:
-        print(allSentences[index])
+        completed_sentence = all_sentences[index][0]
+        source_text = all_sentences[index][1]
+        offset = all_sentences[index][0].index(sub_string)
+        score = getScore(sub_string, completed_sentence)
+        AutoCompleteDataList.append(AutoCompleteData(completed_sentence, source_text, offset, score))
+
+    return AutoCompleteDataList
 
 
-def getCommonSentences(sentencesIndexes):
+def get_common_sentences(sentencesIndexes):
     if len(sentencesIndexes) <= 5:
         return list(sentencesIndexes)
 
     return list(sentencesIndexes)[:5]
 
 
-for sentence in allSentences:
-    insertToDict(sentence)
+for sentence in all_sentences:
+    insert_to_dict(sentence)
 
-fiveAutoComplition()
+get_best_k_completions(input("The system is ready. Enter your text:"))
